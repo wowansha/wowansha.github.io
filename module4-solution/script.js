8e6f23a03a6b23d6fbf8b2fbb6d145997e526495
wowansha <160253490+wowansha@users.noreply.github.com>
2024-02-20 22:59:23 +0800

    mod4

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -29,9 +29,8 @@ WARNING!!! WARNING!!!
 
 */
 
-// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
+// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU) Wrap the entire contents of script.js inside of an IIFE
+// See Lecture 52, part 2-----------done
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
 
